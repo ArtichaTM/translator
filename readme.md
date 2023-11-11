670a7857ec14f73c1998aa8c6ee2001159921eeb
Articha <tima-1324@mail.ru>
2023-11-11 13:51:50 +0300

    Addded __call__ method to Translator Added usage in readme.md

--- readme.md
+++ readme.md
@@ -11,3 +11,22 @@
 1. Выполнить файл check_install.py для установки моделей
 ### Использование
 Всё использование переводчика основывается на вызове методов у класса Translator
+```python
+from translator import Translator
+
+translator = Translator()
+
+text_translate = translator.translate('Как у тебя дела?', 'en')
+text_list = translator.translate_list(['Как у тебя дела?'], 'en')
+text_call_translate = translator('Как у тебя дела?', 'en')
+text_call_list = translator(['Как у тебя дела?'], 'en')
+
+assert text_translate == text_call_translate == text_list[0] == text_call_list[0]
+```
+Узнать все доступные коды языков можно командой Translator.available_codes()
+```python
+from translator import Translator
+
+translator = Translator()
+print(translator.available_codes()) # prints ['ar', 'az', ..., 'zh', 'zt']
+```
--- translator.py
+++ translator.py
@@ -1,8 +1,16 @@
+from typing import Union
+from collections.abc import Iterable
 from logging import warning
 
 from argostranslate.translate import get_translation_from_codes, ITranslation
 from argostranslate import translate
 from argostranslate.package import get_installed_packages
+
+
+__all__ = (
+    'NoSuchLanguage',
+    'Translator'
+)
 
 
 class NoSuchLanguage(Exception):
@@ -31,6 +39,20 @@ class Translator:
                 self._direct_map[package.to_code] = transl_package
             elif package.from_code == 'en':
                 self._transit_map[package.to_code] = transl_package
+        self._transit_map.pop('ru')
+
+    def __call__(self, values: Union[str, Iterable[str]], target: str) -> Union[str, list[str]]:
+        """ High-level translation
+        If values is type of string, returns translated string
+        If values iterable, returns list of translated strings
+        :param values: string or iterable of strings
+        :param target: code of target language
+        :return: string, or list of strings, based on the values argument type
+        """
+        if isinstance(values, str):
+            return self.translate(values, target)
+        if isinstance(values, Iterable):
+            return self.translate_list(values, target)
 
     def translate(self, string: str, target: str) -> str:
         """ Translates from one language to another
@@ -48,7 +70,7 @@ class Translator:
         else:
             raise ValueError('No such language')
 
-    def translate_list(self, strings: list[str], target: str) -> list[str]:
+    def translate_list(self, strings: Iterable[str], target: str) -> list[str]:
         """ Translates list of strings from one language to another
         :param strings: List of strings that needs to be translated
         :param target: Code of target language: en, ru, esp, etc...
@@ -69,5 +91,5 @@ class Translator:
         else:
             warning('Trying to init() class Translator while instance already exists')
 
-
-Translator.init()
+    def available_codes(self) -> list[str]:
+        return sorted([*self._direct_map.keys(), *self._transit_map.keys()])
